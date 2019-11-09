408e6c0e50ed37f0f5ba2e6b44b6eae5893f6d9e
Your Name <you@example.com>
2019-11-09 08:50:29 +0800

    note

--- 7.day(Necessary).py
+++ 7.day(Necessary).py
@@ -384,3 +384,23 @@ b'\xe4\xbd\xa0\xe5\xa5\xbd\n' # 共写入七个字节,在utf8编码中一个汉�
 >>> import getpass as gp
 >>> p = gp.getpass()
 Password:
+#*******************os-module************
+import os
+
+os.getcwd()  # 显示当前路径
+os.listdir()  # ls -a
+os.listdir('/tmp')  # ls -a /tmp
+os.mkdir('/tmp/mydemo')  # mkdir /tmp/mydemo
+os.chdir('/tmp/mydemo')  # cd /tmp/mydemo
+os.listdir()
+os.mknod('test.txt')  # touch test.txt
+os.symlink('/etc/hosts', 'zhuji')  # ln -s /etc/hosts zhuji
+os.path.isfile('test.txt')  # 判断test.txt是不是文件
+os.path.islink('zhuji')  # 判断zhuji是不是软链接
+os.path.isdir('/etc')
+os.path.exists('/tmp')  # 判断是否存在
+os.path.basename('/tmp/abc/aaa.txt')
+os.path.dirname('/tmp/abc/aaa.txt')
+os.path.split('/tmp/abc/aaa.txt')
+os.path.join('/home/tom', 'xyz.txt')
+os.path.abspath('test.txt')  # 返回当前目录test.txt的绝对路径
